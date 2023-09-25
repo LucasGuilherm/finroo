efadcebbaa7fe103daee25a2bc36670365280faa
LucasGuilherm <lucassade110@gmail.com>
2023-09-24 22:52:28 -0300

    fix: erro build

--- src/app/(auth)/accounts/components/accountsList.tsx
+++ src/app/(auth)/accounts/components/accountsList.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { headers } from "next/headers";
 import { fetchApi } from "@/lib/fetchWrap";
 import { CreditCard } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export type conta = {
   id: number;
@@ -12,23 +15,38 @@ export type conta = {
   userId: number;
 };
 
-const AccountsList = async () => {
-  const { listaContas } = await fetchApi<{
-    listaContas: conta[];
-    message?: string;
-  }>("/contas/getContas", {
-    headers: headers(),
-  });
+const AccountsList = () => {
+  const [listaContas, setLista] = useState<conta[]>();
+
+  const getAccounts = async () => {
+    const { listaContas } = await fetchApi<{
+      listaContas: conta[];
+      message?: string;
+    }>("/contas/getContas", {
+      // headers: headers(),
+    });
+
+    setLista(listaContas);
+
+    // return listaContas;
+  };
+
+  useEffect(() => {
+    getAccounts();
+  }, []);
+
+  if (!listaContas) {
+    return <h1>Sem resposta</h1>;
+  }
 
   // await new Promise((r) => setTimeout(r, 2000));
 
   return (
     <div className="flex flex-col gap-4">
       {listaContas.map((conta) => {
-        console.log(conta);
-
         return (
           <Link
+            key={conta.id}
             href={`./accounts/${conta.id}`}
             className="bg-slate-200 flex flex-row gap-3 p-4 rounded-lg"
           >
